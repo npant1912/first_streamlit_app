a2eee098888bc9a246ee4e7314ec8a0e85283239
npant1912 <78775528+npant1912@users.noreply.github.com>
2023-09-20 18:53:49 +0530

    Update steamlit_app.py

--- steamlit_app.py
+++ steamlit_app.py
@@ -24,12 +24,13 @@ try:
     streamlit.dataframe(back_from_function)
 except URLError as e:
     streamlit.error()
-streamlit.stop()
+#streamlit.stop()
 
-streamlit.header("Fruityvice Fruit Add!")
-add_my_fruit = streamlit.text_input('What fruit would you like to add?','Kiwi')
-streamlit.write('Thanks for adding ', add_my_fruit)
+#streamlit.header("Fruityvice Fruit Add!")
+#add_my_fruit = streamlit.text_input('What fruit would you like to add?','Kiwi')
+#streamlit.write('Thanks for adding ', add_my_fruit)
 
+streamlit.header("The fruit load list contains:")
 def get_fruit_load_list():
   with my_cnx.cursor() as my_cur:
        my_cur.execute("select * from fruit_load_list")
