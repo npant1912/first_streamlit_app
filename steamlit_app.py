6e7c09f6bedb4a707de3b621b79233cc1af30d9c
npant1912 <78775528+npant1912@users.noreply.github.com>
2023-09-20 18:15:39 +0530

    Update steamlit_app.py

--- steamlit_app.py
+++ steamlit_app.py
@@ -11,9 +11,8 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 streamlit.dataframe(fruits_to_show)
 streamlit.header("Fruityvice Fruit Advice!")
 def get_fruityvice_data(this_fruit_choice):
-	fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+(this_fruit_choice))
-    	fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-	return fruityvice_normalized 
+  back_from_function = get_fruityvice_data(fruit_choice)
+  steamlit.dataframe(back_from_function)
 try:
   fruit_choice = streamlit.text_input('What fruit would you like information about?')
   if not fruit_choice:
