37ffa13a3fc6a542e674477b520d6b63546d309b
npant1912 <78775528+npant1912@users.noreply.github.com>
2023-09-20 16:07:42 +0530

    Update steamlit_app.py

--- steamlit_app.py
+++ steamlit_app.py
@@ -9,6 +9,7 @@ streamlit.dataframe(fruits_to_show)
 streamlit.header("Fruityvice Fruit Advice!")
 fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
 streamlit.write('The user entered ', fruit_choice)
+
 import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+fruit_choice)
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
@@ -22,5 +23,5 @@ my_cur = my_cnx.cursor()
 my_cur.execute("select * from fruit_load_list")
 my_data_row = my_cur.fetchall()
 streamlit.header("The fruit load list contains:")
-streamlit.dataframe(my_data_row)
+streamlit.dataframe(add_my_fruit)
 
