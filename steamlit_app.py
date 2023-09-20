be1dd4a5cad0e8f3438dd3d9712fdd2c5d398e2e
npant1912 <78775528+npant1912@users.noreply.github.com>
2023-09-20 18:19:53 +0530

    Update steamlit_app.py

--- steamlit_app.py
+++ steamlit_app.py
@@ -9,10 +9,10 @@ my_fruit_list = my_fruit_list.set_index('Fruit')
 fruits_selected = streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index),['Avocado','Strawberries'])
 fruits_to_show = my_fruit_list.loc[fruits_selected]
 streamlit.dataframe(fruits_to_show)
-streamlit.header("Fruityvice Fruit Advice!")
 def get_fruityvice_data(this_fruit_choice):
   back_from_function = get_fruityvice_data(fruit_choice)
   steamlit.dataframe(back_from_function)
+streamlit.header("Fruityvice Fruit Advice!")
 try:
   fruit_choice = streamlit.text_input('What fruit would you like information about?')
   if not fruit_choice:
